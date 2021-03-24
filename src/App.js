d63023498500efe0abd02071450dc954a1de5657
Sc0tticus <ssinger303@gmail.com>
2021-03-23 22:17:36 -0600

    Monster names now appear in 4 columns

--- src/App.js
+++ src/App.js
@@ -35,11 +35,10 @@ class App extends Component {
 		return (
 			<div className="App">
 				<CardList name="Scott">
-					<h1>Scott</h1>
+					{this.state.monsters.map(monster => (
+						<h1 key={monster.id}>{monster.name}</h1>
+					))}
 				</CardList>
-				{this.state.monsters.map(monster => (
-					<h1 key={monster.id}>{monster.name}</h1>
-				))}
 			</div>
 		);
 	}
